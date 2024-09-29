3ea79367d32d39792116064be1250d8a37df3644
RonexLemon <ronexondimu@gmail.com>
2024-09-29 20:36:05 +0300

    load file

---
+++ manage/config/envload.go
@@ -0,0 +1,17 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func EnvLoad()string{
+	err:= godotenv.Load()
+	if err !=nil{
+		log.Fatal("Error loading .env files")
+	}
+
+	return os.Getenv("DB_Mongo_URL")
+}
--- manage/models/task.go
+++ manage/models/task.go
@@ -1,5 +1,27 @@
 package models
 
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type TaskStatus string
+const (Pending TaskStatus = "Pending"
+Completed TaskStatus = "Completed"
+Failed TaskStatus = "In-progress"
+
+)
 type Task struct{
-	
+	Task_id  primitive.ObjectID `json:"task_id" bson:"task_id"`
+	Title       string    `json:"title"`
+    Description string    `json:"description"`
+    Status      TaskStatus    `json:"status"` // pending, in-progress, completed
+    DueDate     time.Time `json:"due_date"`
+    Priority    string    `json:"priority"` // low, medium, high
+    User  primitive.ObjectID  `json:"user_id"` // User ID
+    
+    CreatedAt   time.Time `json:"created_at"`
+    UpdatedAt   time.Time `json:"updated_at"`
+
 }
