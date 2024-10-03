61772b8cbafa75782c67b775d591bcf6d8460db0
RonexLemon <ronexondimu@gmail.com>
2024-10-03 09:13:42 +0300

    db config

--- jobquest/config/db.go
+++ jobquest/config/db.go
@@ -1,7 +1,31 @@
 package config
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"context"
+	"log"
+	"time"
 
-func DbConnect() *mongo.Client{
-	
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DbConnect() *mongo.Client {
+	client, err := mongo.NewClient(options.Client().ApplyURI(LoadEnv()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//ping
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return client
+
 }
